05e108b2bce9d450c4c86bec90cdd0eff0203057
Nramyanaga <ramyanaganagarur@gmail.com>
2025-09-15 00:01:00 -0400

    added lines 3,4,5,6,7,8,9 so it can be tested on swaggerhub to Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,5 +1,12 @@
 <?php
     require_once __DIR__ . '/config.php';
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Allow-Methods: POST, OPTIONS');
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(204); // No Content
+        exit(0);
+    }
     $inData = getRequestInfo();
     
     $id = 0;
